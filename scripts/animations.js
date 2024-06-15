59ab3f6fe7b04c7d9a22feb35102db9c27aae5cd
Andrey <teryaev.2004@mail.ru>
2024-06-15 05:56:31 +0300

    Added some blocks and animations for their

---
+++ scripts/animations.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from(".about__text.left", {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top 80%",
+      toggleActions: "play none none reverse",
+    },
+  });
+
+  gsap.from(".about__text.right", {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top 80%",
+      toggleActions: "play none none reverse",
+    },
+  });
+
+  gsap.to(".about__content-img", {
+    scale: 0.8,
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top 80%",
+      end: "bottom top",
+      scrub: true,
+    },
+  });
+
+  ScrollTrigger.create({
+    trigger: ".about",
+    start: "top 80%",
+    end: "bottom top",
+    scrub: true,
+    onEnter: () => document.querySelector(".about").classList.add("change-bg"),
+    onLeaveBack: () =>
+      document.querySelector(".about").classList.remove("change-bg"),
+  });
+
+  gsap.from(".geography__item", {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    ease: "bounce.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".geography",
+      start: "top 80%",
+    },
+  });
+
+  // Counter animation for geography__item-count
+  const counters = document.querySelectorAll(".geography__item-count");
+  counters.forEach((counter) => {
+    const targetCount = parseInt(counter.dataset.count);
+    const sign = counter.dataset.sign || "";
+
+    gsap.fromTo(
+      counter,
+      {
+        innerText: 0,
+      },
+      {
+        innerText: targetCount,
+        duration: 2,
+        ease: "power1.in",
+        scrollTrigger: {
+          trigger: counter,
+          start: "top 80%",
+        },
+        snap: { innerText: 1 },
+        onUpdate: function () {
+          counter.innerText = `${Math.ceil(counter.innerText)}${sign}`;
+        },
+      }
+    );
+  });
+});
